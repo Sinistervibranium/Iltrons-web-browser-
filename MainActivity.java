f5a36c05576077a13061e3c18932399646183b1c
Sinistervibranium <141748447+Sinistervibranium@users.noreply.github.com>
2025-04-12 03:36:15 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+
+package com.iltrons.os;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // Load content from GitHub
+        webView.loadUrl("https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/index.html");
+
+        setContentView(webView);
+    }
+}
